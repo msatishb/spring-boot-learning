ee678eee5941de02fe28d190e31d908d3f4d817a
msatishb <satish.myneni369@gmail.com>
2019-08-19 08:05:14 +0530

    This is a JDBC example for local testing and connection to the Database like ORACLE

---
+++ JDBCConnectionExample.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCConnectionExample {
+	public static void main(String args[]) throws SQLException{
+		Connection con = null;
+		try{  
+		Class.forName("oracle.jdbc.driver.OracleDriver");  
+		con =DriverManager.getConnection(  
+		"jdbc:oracle:thin:@localhost:1521:xe","system","satish");  
+		//here sonoo is database name, root is username and password  
+		Statement stmt=con.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from emp");  
+		while(rs.next())  
+		System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3) +" "+ rs.getString(4)
+		+" "+rs.getDate(5) + " "+ rs.getInt(6) );  
+		}catch(Exception e){ 
+			System.out.println(e);
+			}
+		finally {
+			con.close();
+		}
+		}
+}
